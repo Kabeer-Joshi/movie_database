0a547b4f6dc1c37bc98e8d4d265e0bd1a6bc4573
Kabeer-Joshi <kabeer@goreeva.com>
2024-06-12 16:01:12 +0530

    JWT login and refresh token

--- movies/views.py
+++ movies/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view ,permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Movie
 from .serializers import MovieSerializer
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def movie_list(request):
     movies = Movie.objects.all()
     serializer = MovieSerializer(movies, many=True)
