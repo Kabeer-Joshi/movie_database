797199ae0f8a16ae8154d1bbe42f22ea9d818c7d
Kabeer-Joshi <kabeer@goreeva.com>
2024-06-12 16:18:04 +0530

    Registeration done

--- movie_database/urls.py
+++ movie_database/urls.py
@@ -5,10 +5,12 @@ from rest_framework_simplejwt.views import (
 
 from django.contrib import admin
 from django.urls import include , path
+from movies.views import registration_view
 
 urlpatterns = [
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/register/' ,registration_view , name='register'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path('admin/', admin.site.urls),
