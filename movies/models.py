ee525b37e487ef7278a73e2bcb56c8d681eacb53
Kabeer-Joshi <kabeer@goreeva.com>
2024-08-01 20:56:12 +0530

    Notification model

--- movies/models.py
+++ movies/models.py
@@ -48,3 +48,19 @@ class Watchlist(models.Model):
     def __str__(self):
         return f'{self.user.username} - {self.movie.title}'
 
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.message[:50]}'
+
+
+
+
+
+
+
+
